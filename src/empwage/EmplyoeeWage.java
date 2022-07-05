0d1d1c80b758625e1ac0bce0795e7f591a4c6cb5
priya <priyapandey8884.blr@gmail.com>
2022-07-05 16:06:01 +0530

    Added:Emplyoee is Present Or Absent

--- src/empwage/EmplyoeeWage.java
+++ src/empwage/EmplyoeeWage.java
@@ -4,7 +4,14 @@ public class EmplyoeeWage {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Emplyoee Wage Program");
-
-	}
+		
+		int Is_present = 1;
+		double empcheck = Math.floor((Math.random()*100)%2);
+		if(empcheck == Is_present) 
+			System.out.println("Emplyoee is present");
+		else
+			System.out.println("Emplyoee is absent");
+		
+		}
 
 }
