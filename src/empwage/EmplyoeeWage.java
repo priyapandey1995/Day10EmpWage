1b58020afea7e97395fc2f718e1679ce3e7c33ae
priya <priyapandey8884.blr@gmail.com>
2022-07-05 16:19:55 +0530

    Added:Emplyoee daily Wage

--- src/empwage/EmplyoeeWage.java
+++ src/empwage/EmplyoeeWage.java
@@ -3,14 +3,22 @@ package empwage;
 public class EmplyoeeWage {
 
 	public static void main(String[] args) {
-		System.out.println("Welcome to Emplyoee Wage Program");
 		
 		int Is_present = 1;
-		double empcheck = Math.floor((Math.random()*100)%2);
-		if(empcheck == Is_present) 
+		int wagePerHour = 20;
+		int fullDayHour = 8;
+		int dailywage = wagePerHour * fullDayHour;
+		double empcheck = Math.floor((Math.random()*100)%3);
+		if(empcheck == 1) {
 			System.out.println("Emplyoee is present");
-		else
+			System.out.println("Daily wage of Emplyoee is" + dailywage);
+		}	
+		else if(empcheck == 0 ){
 			System.out.println("Emplyoee is absent");
+		}
+		else {
+			System.out.println("Emplyoee is present for the half day");
+		}
 		
 		}
 
